cbfef6b649b59947c2d857578cbb88f2e280d1f8
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-08-18 20:22:10 +0000

    Add review form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+    //Materialize mobile navbar
     $('.sidenav').sidenav({edge: "right"});
+    //Materialize accordion for reviews//
     $('.collapsible').collapsible();
+    // Materialize Autocomplete Form Input //
+    $('input.autocomplete').autocomplete({
+        data: {
+            // need to add mongo.db.catalogue.game_title here somehow //
+        },
+    });
+    $('select').formSelect();
 });
