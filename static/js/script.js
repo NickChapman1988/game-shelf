eb1b89694b8a64baef38105f218866962e6538e3
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-09-07 09:04:58 +0000

    Hide clear icon with no search input

--- static/js/script.js
+++ static/js/script.js
@@ -51,3 +51,18 @@ function validateMaterializeSelect() {
 function clearSearch(){
     document.getElementById("query").value = "";
 }
+
+Array.prototype.forEach.call(document.querySelectorAll('.clearable-input'), function(el) {
+    var input = el.querySelector('input');
+    conditionallyHideClearIcon();
+    input.addEventListener('input', conditionallyHideClearIcon);
+    el.querySelector('[data-clear-input]').addEventListener('click', function(e) {
+      input.value = '';
+      conditionallyHideClearIcon();
+    });
+  
+    function conditionallyHideClearIcon(e) {
+      var target = (e && e.target) || input;
+      target.nextElementSibling.style.display = target.value ? 'inline-block' : 'none';
+    }
+});
